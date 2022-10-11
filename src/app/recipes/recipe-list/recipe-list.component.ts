f0038c8db1e26e89aff8ad06cda0cc17069b07fd
Jelle <jelle@famvandenbrink.net>
2022-10-11 15:15:18 +0200

    recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Pannenkoeken', 'Lekkere pannenkoeken van heit', 'https://www.lekkerensimpel.com/wp-content/uploads/2016/10/IMG_3947.jpg')
+  ];
 
   constructor() { }
 

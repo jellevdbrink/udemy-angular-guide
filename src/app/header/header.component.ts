0c2a230c5faf36321c7e89f2073a5d10c676bd4f
Jelle <jelle@famvandenbrink.net>
2022-10-18 11:39:33 +0200

    more work on recipes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
   selector: 'app-header',
@@ -6,4 +6,13 @@ import { Component } from "@angular/core";
 })
 export class HeaderComponent {
   collapsed: boolean = true;
+  @Output() navEvent = new EventEmitter<string>();
+
+  toRecipes() {
+     this.navEvent.emit('recipes');
+  }
+
+  toShoppingList() {
+    this.navEvent.emit('shoppingList');
+  }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -10,10 +10,15 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('Pannenkoeken', 'Lekkere pannenkoeken van heit', 'https://www.lekkerensimpel.com/wp-content/uploads/2016/10/IMG_3947.jpg')
   ];
+  @Output() clickRecipe = new EventEmitter<Recipe>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  handleRecipeClick(recipe: Recipe) {
+    this.clickRecipe.emit(recipe);
   }
 
 }
